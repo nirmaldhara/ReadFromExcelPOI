f6660fedc32c561a6ece3947b7788023b0062fe3
Nirmal <nirmal.dhara@gmail.com>
2016-05-01 20:26:35 +0530

    change for read excel
    poi read from excel

--- src/com/javaant/ReadFromEXcel.java
+++ src/com/javaant/ReadFromEXcel.java
@@ -1,95 +1,83 @@
 package com.javaant;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadFromEXcel {
 
+
 	static String filepath = null;
-	Object[][] data = null;
+	static Object[][] data = null;
+
 	public static void main(String ar[]) {
 		ReadFromEXcel rw = new ReadFromEXcel("D:\\Java_Ant_Post\\readWritexls-poi\\ReadWriteExcel\\excels\\abc.xlsx");
-		rw.writeDataToExcel(filepath);
-		//rw.readDataFromExcel();
+		data = rw.readDataFromExcel();
+		// rw.readDataFromExcel();
 
 	}
+
 	public ReadFromEXcel(String filepath) {
 		this.filepath = filepath;
+	}
+
+	public Object[][] readDataFromExcel() {
+		final DataFormatter df = new DataFormatter();
+		try {
+
+			FileInputStream file = new FileInputStream(getFile());
+			// Create Workbook instance holding reference to .xlsx file
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			// Get workbook
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			// Iterate through each rows one by one
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			int rownum = 0;
+			int colnum = 0;
+			// ignore the first row , it may be header
+			Row r = rowIterator.next();
+
+			int rowcount = sheet.getLastRowNum();
+			int colcount = r.getPhysicalNumberOfCells();
+			data = new Object[rowcount][colcount];
+
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+
+				Iterator<Cell> cellIterator = row.cellIterator();
+				colnum = 0;
+				while (cellIterator.hasNext()) {
+
+					Cell cell = cellIterator.next();
+					// Check the cell type and format accordingly
+					data[rownum][colnum] = df.formatCellValue(cell);
+					System.out.print(" " + df.formatCellValue(cell));
+					colnum++;
+				}
+				rownum++;
+				System.out.println("");
+			}
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return data;
 	}
 
 	public File getFile() throws FileNotFoundException {
 		File here = new File(filepath);
 		return new File(here.getAbsolutePath());
 
-	}
-
-	
-	
-	private static void writeToCell(int rowno, int colno, XSSFSheet sheet, String val) {
-		try {
-			sheet.getRow(rowno);
-			XSSFRow row = sheet.getRow(rowno);
-			if (row == null) {
-				row = sheet.createRow(rowno);
-			}
-			XSSFCell cell = row.createCell(colno);
-			cell.setCellValue(val);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	
-	public static void writeDataToExcel(String file) {
-		XSSFWorkbook wb = null;
-		XSSFSheet sheet = null;
-		FileOutputStream fileOut = null;
-		
-		
-			String excelFileName = file;
-
-			String sheetName = "Sheet1";//name of sheet
-
-			wb = new XSSFWorkbook();
-			sheet = wb.createSheet(sheetName);
-			DecimalFormat df2 = new DecimalFormat(".##");
-			try{
-
-	
-			
-			writeToCell(0, 0, sheet,  "This is one");// row 1 column 1
-			writeToCell(0, 1, sheet,  "this is two");// row 1 column 2
-			writeToCell(1, 0, sheet,  "this is three");// row 2 column 1
-			writeToCell(1, 1, sheet,  "this is four");// row 2 column 2
-			int r = 4;
-			
-			System.out.println("working fine");
-			fileOut = new FileOutputStream(excelFileName);
-			wb.write(fileOut);
-
-			//write this workbook to an Outputstream.
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-
-			try {
-				fileOut.flush();
-				fileOut.close();
-			} catch (IOException ex) {
-				Logger.getLogger(ReadFromEXcel.class.getName()).log(Level.SEVERE, null, ex);
-			}
-
-		}
 	}
 
 	
